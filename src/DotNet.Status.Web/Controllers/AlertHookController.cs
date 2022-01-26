dotnet/dnceng

commit 047595742948451576bad41e81595f103a23d141

Do not include graph image in issue when Grafana alert doesn't have it (#1803)

src/DotNet.Status.Web/Controllers/AlertHookController.cs
@@ -148,6 +148,7 @@ namespace DotNet.Status.Web.Controllers
             }
             
             string icon = GetIcon(notification);
+            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? "![Metric Graph]({notification.ImageUrl})" : string.Empty;
 
             return $@":{icon}: Metric state changed to *{notification.State}*
 
@@ -155,7 +156,7 @@ namespace DotNet.Status.Web.Controllers
 
 {metricText}
 
-![Metric Graph]({notification.ImageUrl})
+{image}
 
 [Go to rule]({notification.RuleUrl})".Replace("\r\n","\n");
         }
@@ -169,6 +170,7 @@ namespace DotNet.Status.Web.Controllers
             }
 
             string icon = GetIcon(notification);
+            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? "![Metric Graph]({notification.ImageUrl})" : string.Empty;
 
             string issueTitle = notification.Title;
 
@@ -187,7 +189,7 @@ namespace DotNet.Status.Web.Controllers
 
 {metricText}
 
-![Metric Graph]({notification.ImageUrl})
+{image}
 
 [Go to rule]({notification.RuleUrl})
 

commit 3cc6186ce1a51ee1d628263992f14785cb8c0df5

Fix displaying Grafana alert issue images (#1813)
Introduced in #1803

src/DotNet.Status.Web/Controllers/AlertHookController.cs
@@ -148,7 +148,7 @@ namespace DotNet.Status.Web.Controllers
             }
             
             string icon = GetIcon(notification);
-            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? "![Metric Graph]({notification.ImageUrl})" : string.Empty;
+            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? $"![Metric Graph]({notification.ImageUrl})" : string.Empty;
 
             return $@":{icon}: Metric state changed to *{notification.State}*
 
@@ -170,7 +170,7 @@ namespace DotNet.Status.Web.Controllers
             }
 
             string icon = GetIcon(notification);
-            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? "![Metric Graph]({notification.ImageUrl})" : string.Empty;
+            string image = !string.IsNullOrEmpty(notification.ImageUrl) ? $"![Metric Graph]({notification.ImageUrl})" : string.Empty;
 
             string issueTitle = notification.Title;
 