5b57925fe501f042a6ba49eea1471c4e881f4af7
Missy Messa <mjanecke@microsoft.com>
2023-07-12 12:34:43 -0700

    whoops

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -9,4 +9,4 @@ Set-StrictMode -Version 2.0
 New-LocalGroup -Name "DncEngConfigurationUsers" -ErrorAction Continue
 Add-LocalGroupMember -Group "DncEngConfigurationUsers" -Member $(whoami) -ErrorAction Continue
 
-dotnet run --project .\src\Microsoft.DncEng.Configuration.Bootstrap\Microsoft.DncEng.Configuration.Bootstrap.csproj -- -r "https://vault.azure.net"-r "https://management.azure.com"
+dotnet run --project .\src\Microsoft.DncEng.Configuration.Bootstrap\Microsoft.DncEng.Configuration.Bootstrap.csproj -- -r "https://vault.azure.net" -r "https://management.azure.com"
