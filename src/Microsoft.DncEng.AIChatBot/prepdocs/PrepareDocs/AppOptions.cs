09a98abbffce59c70991872339ba33b88ac29b74
Jeremiah Lipscomb <t-jelipscomb@microsoft.com>
2023-07-13 15:49:16 -0700

    Added the prepdocs project for the chatbot

---
+++ src/Microsoft.DncEng.AIChatBot/prepdocs/PrepareDocs/AppOptions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace PrepareDocs;
+
+internal record class AppOptions(
+    string Files,
+    string? Category,
+    bool SkipBlobs,
+    string? StorageServiceBlobEndpoint,
+    string? Container,
+    string? TenantId,
+    string? SearchServiceEndpoint,
+    string? SearchIndexName,
+    bool Remove,
+    bool RemoveAll,
+    string? FormRecognizerServiceEndpoint,
+    bool Verbose,
+    IConsole Console) : AppConsole(Console);
+
+internal record class AppConsole(IConsole Console);
