be6d7bfbc78cc0d0991ed18362489e1faa7edfa0
Jeremiah Lipscomb <t-jelipscomb@microsoft.com>
2023-08-07 08:22:55 -0700

    Added azure chatbot code

---
+++ src/Microsoft.DncEng.AzureAIChatbot/frontend/src/api/api.ts
@@ -0,0 +1,27 @@
+import { UserInfo, ConversationRequest } from "./models";
+
+export async function conversationApi(options: ConversationRequest, abortSignal: AbortSignal): Promise<Response> {
+    const response = await fetch("/conversation", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            messages: options.messages
+        }),
+        signal: abortSignal
+    });
+
+    return response;
+}
+
+export async function getUserInfo(): Promise<UserInfo[]> {
+    const response = await fetch('/.auth/me');
+    if (!response.ok) {
+        console.log("No identity provider found. Access to chat will be blocked.")
+        return [];
+    }
+
+    const payload = await response.json();
+    return payload;
+}
---
+++ src/Microsoft.DncEng.AzureAIChatbot/frontend/src/api/models.ts
@@ -0,0 +1,58 @@
+export type AskResponse = {
+    answer: string;
+    citations: Citation[];
+    error?: string;
+};
+
+export type Citation = {
+    content: string;
+    id: string;
+    title: string | null;
+    filepath: string | null;
+    url: string | null;
+    metadata: string | null;
+    chunk_id: string | null;
+    reindex_id: string | null;
+}
+
+export type ToolMessageContent = {
+    citations: Citation[];
+    intent: string;
+}
+
+export type ChatMessage = {
+    role: string;
+    content: string;
+    end_turn?: boolean;
+};
+
+export enum ChatCompletionType {
+    ChatCompletion = "chat.completion",
+    ChatCompletionChunk = "chat.completion.chunk"
+}
+
+export type ChatResponseChoice = {
+    messages: ChatMessage[];
+}
+
+export type ChatResponse = {
+    id: string;
+    model: string;
+    created: number;
+    object: ChatCompletionType;
+    choices: ChatResponseChoice[];
+    error?: any;
+}
+
+export type ConversationRequest = {
+    messages: ChatMessage[];
+};
+
+export type UserInfo = {
+    access_token: string;
+    expires_on: string;
+    id_token: string;
+    provider_name: string;
+    user_claims: any[];
+    user_id: string;
+};
