b0c1594deb8951bb94c60c713cbcff38dd1c1990
missymessa <mjanecke@microsoft.com>
2023-08-30 09:47:59 -0700

    fixing some tests

--- src/DotNet.Status.Web/DotNet.Status.Web.Tests/GitHubHookControllerTests.cs
+++ src/DotNet.Status.Web/DotNet.Status.Web.Tests/GitHubHookControllerTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using DotNet.Status.Web.Controllers;
