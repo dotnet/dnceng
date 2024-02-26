9a8e7301b882d266d07cd610f9897c19ee58d503
Alitzel Mendez <alichel_6288@hotmail.com>
2024-02-26 11:38:35 -0800

    Fix models for githu hook controller

--- src/DotNet.Status.Web/DotNet.Status.Web/Models/IssueCommentPayloadWithChanges.cs
+++ src/DotNet.Status.Web/DotNet.Status.Web/Models/IssueCommentPayloadWithChanges.cs
@@ -2,8 +2,12 @@ using Octokit;
 
 namespace DotNet.Status.Web.Models;
 
-public class IssueCommentPayloadWithChanges : IssueCommentPayload
+public class IssueCommentPayloadWithChanges 
 {
     public IssueOrPullRequestCommentChanges Changes { get; set; }
-
+    public string Action { get;  set; }
+    public User  User{ get;  set; }
+    public Issue Issue { get; set; }
+    public Repository Repository { get; set; }
+    public IssueComment Comment { get; set; }
 }
