1f16af02e59d73b7cc66cc82c4e1fa5b5c78546e
missymessa <mjanecke@microsoft.com>
2024-06-18 10:49:57 -0700

    Trying a different value

--- src/SecretManager/Microsoft.DncEng.SecretManager.ScenarioTests/ScenarioTestsBase.cs
+++ src/SecretManager/Microsoft.DncEng.SecretManager.ScenarioTests/ScenarioTestsBase.cs
@@ -25,7 +25,7 @@ namespace Microsoft.DncEng.SecretManager.Tests
         // Expect credentials for the Service Principal used in these tests to be set
         // in enviornment variables.
         private readonly TokenCredential _tokenCredential = new ClientSecretCredential(
-            Environment.GetEnvironmentVariable("ARM_TENANT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_ID"), Environment.GetEnvironmentVariable("ARM_ID_TOKEN")); //EnvironmentCredential();
+            Environment.GetEnvironmentVariable("ARM_TENANT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_KEY")); //EnvironmentCredential();
 
         protected async Task ExecuteSynchronizeCommand(string manifest)
         {
