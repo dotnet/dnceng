23b27aa405f177f9de6e10914fb338d48c0051b4
missymessa <mjanecke@microsoft.com>
2024-06-18 14:55:51 -0700

    Try a different credential type

--- src/SecretManager/Microsoft.DncEng.SecretManager.ScenarioTests/ScenarioTestsBase.cs
+++ src/SecretManager/Microsoft.DncEng.SecretManager.ScenarioTests/ScenarioTestsBase.cs
@@ -24,8 +24,10 @@ namespace Microsoft.DncEng.SecretManager.Tests
 
         // Expect credentials for the Service Principal used in these tests to be set
         // in enviornment variables.
-        private readonly TokenCredential _tokenCredential = new ClientSecretCredential(
-            Environment.GetEnvironmentVariable("ARM_TENANT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_KEY")); //EnvironmentCredential();
+        private readonly TokenCredential _tokenCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = Environment.GetEnvironmentVariable("ARM_CLIENT_ID") });
+            
+            //new ClientSecretCredential(
+            //Environment.GetEnvironmentVariable("ARM_TENANT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_ID"), Environment.GetEnvironmentVariable("ARM_CLIENT_KEY")); //EnvironmentCredential();
 
         protected async Task ExecuteSynchronizeCommand(string manifest)
         {
