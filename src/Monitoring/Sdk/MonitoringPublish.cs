c84a512e273108b8dac4a1cf4f24a389d64da736
Michael Stuckey <Michael.Stuckey@microsoft.com>
2024-06-28 16:48:16 -0700

    Continue to support client secret auth to allow dependants to migrate without breakage

--- src/Monitoring/Sdk/MonitoringPublish.cs
+++ src/Monitoring/Sdk/MonitoringPublish.cs
@@ -32,11 +32,14 @@ public class MonitoringPublish : BuildTask
     [Required]
     public string KeyVaultName { get; set; }
 
+    // For azure pipeline service connection authentication
     public string ClientId { get; set; }
-
     public string ServiceConnectionId { get; set; }
-    
     public string SystemAccessToken { get; set; }
+
+    //  For client secret authentication
+    public string KeyVaultServicePrincipalId { get; set; }
+    public string KeyVaultServicePrincipalSecret { get; set; }
 
     [Required]
     public string Tag { get; set; }
@@ -58,25 +61,39 @@ public class MonitoringPublish : BuildTask
         string msftTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
         TokenCredential tokenCredential;
 
-        if (ClientId == null && ServiceConnectionId == null && SystemAccessToken == null)
+        if (ClientId == null && ServiceConnectionId == null && SystemAccessToken == null && KeyVaultServicePrincipalId == null && KeyVaultServicePrincipalSecret == null)
         {
             tokenCredential = new DefaultAzureCredential(
-                new DefaultAzureCredentialOptions() 
-                { 
-                    TenantId = msftTenantId 
+                new DefaultAzureCredentialOptions()
+                {
+                    TenantId = msftTenantId
                 }
             );
         }
-        else if (ClientId == null || ServiceConnectionId == null || SystemAccessToken == null)
+        else if (ClientId != null || ServiceConnectionId != null || SystemAccessToken != null)
         {
-            Log.LogError("Invalid login combination. Set ClientId, ServiceConnectionId and SystemAccessToken for CI, or none for local user authentication.");
-            return false;
+            if (ClientId == null || ServiceConnectionId == null || SystemAccessToken == null)
+            {
+                Log.LogError("Invalid login combination. Set ClientId, ServiceConnectionId and SystemAccessToken for CI, or none for local user authentication.");
+                return false;
+            }
+            else
+            {
+                tokenCredential = new AzurePipelinesCredential(msftTenantId, ClientId, ServiceConnectionId, SystemAccessToken);
+            }
         }
         else
         {
-            tokenCredential = new AzurePipelinesCredential(msftTenantId, ClientId, ServiceConnectionId, SystemAccessToken);
+            if (KeyVaultServicePrincipalId == null || KeyVaultServicePrincipalSecret == null)
+            {
+                Log.LogError("Invalid login combination. Set KeyVaultServicePrincipalId and KeyVaultServicePrincipalSecret for Client Secret authentication.");
+                return false;
+            }
+            else
+            {
+                tokenCredential = new ClientSecretCredential(msftTenantId, KeyVaultServicePrincipalId, KeyVaultServicePrincipalSecret);
+            }
         }
-
 
         using (var client = new GrafanaClient(Host, AccessToken))
         using (var deploy = new DeployPublisher(
