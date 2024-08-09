77e3e8c3807da8c9439ac2c83cb4613879518d6c
Cali Kuang <t-calikuang@microsoft.com>
2024-08-09 11:50:11 -0700

    Update welcome message test

--- src/Chatbot/Chatbot.Tests/Bots/GreetingTests.cs
+++ src/Chatbot/Chatbot.Tests/Bots/GreetingTests.cs
@@ -1,14 +1,14 @@
 // Based off Bot Builder V4 SDK Template for Visual Studio CoreBot v4.22.0
 
-using ChatbotTests.Bots;
-using ChatbotTests.Tests.Common;
+//using Chatbot.Tests.Bots;
+//using Chatbot.Tests.Common;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Adapters;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -17,12 +17,48 @@ namespace Chatbot.Tests.Bots
 {
     public class GreetingTests
     {
-        [Fact]
-        public void TestHelloWorldString()
+        private readonly Dictionary<string, string> _cards = new Dictionary<string, string>()
         {
-            string expected = "Hello world";
-            string actual = "Hello world";
-            Assert.Equal(expected, actual);
+            {"FeedbackCard", @"C:\Users\t-calikuang\source\repos\dnceng\src\Chatbot\Chatbot\Resources\FeedbackCard.json"},
+            {"ContactSheet", @"C:\Users\t-calikuang\source\repos\dnceng\src\Chatbot\Chatbot\Resources\ContactSheet.json"},
+            {"WelcomeCard", @"C:\Users\t-calikuang\source\repos\dnceng\src\Chatbot\Chatbot\Resources\WelcomeCard.json"}
+        };
+
+        [Fact]
+        public async Task TestGreetingSent()
+        {
+            // Set up: Mock telemetry client, configuration, turn context
+            Mock<ILogger<ChatbotForDNCEng>> mockLogger = new Mock<ILogger<ChatbotForDNCEng>>();
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            Mock<ITurnContext> mockTurnContext = new Mock<ITurnContext>();
+            Activity activity = new Activity
+            {
+                Type = ActivityTypes.ConversationUpdate,
+                MembersAdded = new List<ChannelAccount> { new ChannelAccount() { Id="user1", Name="User one", Role="user"} },
+                Recipient = new ChannelAccount() { Id = "bot1", Name = "Bot one", Role = "bot" }
+            };
+            mockTurnContext.Setup(tc => tc.Activity).Returns(activity);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            ChatbotForDNCEng bot = new ChatbotForDNCEng(mockLogger.Object, mockConfiguration.Object);
+
+            // Expected value
+            Attachment expectedCard = ChatbotForDNCEng.CreateAdaptiveCardAttachment(_cards["WelcomeCard"]);
+
+            // Act
+            // Send the conversation update activity to the bot.
+            //await bot.SendWelcomeMessageAsync(mockTurnContext.Object, cancellationToken);
+            await bot.OnTurnAsync(mockTurnContext.Object, cancellationToken);
+
+            // Verify
+            // I think it should be twice, once for the bot to enter the channel and once for the user
+            var expectedActivity = MessageFactory.Attachment(expectedCard);
+            mockTurnContext.Verify(context => context.SendActivityAsync(expectedActivity,
+            It.IsAny<CancellationToken>()), Times.Once);
+
+
         }
     }
 }
