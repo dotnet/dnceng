834f9512a1bbdba5225e335de9067de1f0fcbe3b
calik2 <122586425+calik2@users.noreply.github.com>
2024-08-26 15:24:38 -0700

    Merge deployed bot (#3742)
    Co-authored-by: Cali Kuang <t-calikuang@microsoft.com>
    Co-authored-by: Michael Stuckey <michael.stuckey@microsoft.com>

---
+++ src/Chatbot/Chatbot.Tests/Bots/GreetingTests.cs
@@ -0,0 +1,179 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Chatbot;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+
+namespace CoreBot.Tests.Bots
+{
+    public class DialogAndWelcomeBotTests
+    {
+        private readonly Dictionary<string, string> _cards = new Dictionary<string, string>();
+        public DialogAndWelcomeBotTests() 
+        {
+            
+            var basePath = Path.GetDirectoryName(typeof(ChatbotForDNCEng).Assembly.Location);
+            basePath ??= "";
+
+            string resourcesPath = Path.GetFullPath(Path.Combine(basePath, "Resources"));
+
+            foreach (string filePath in Directory.EnumerateFiles(resourcesPath, "*.json", SearchOption.AllDirectories))
+            {
+                _cards.Add(Path.GetFileNameWithoutExtension(filePath), filePath);
+            }
+
+        }
+
+        [Fact]
+        public async Task ReturnsWelcomeCardOnConversationUpdate()
+        {
+            // Arrange
+            Mock<ILogger<ChatbotForDNCEng>> mockLogger = new Mock<ILogger<ChatbotForDNCEng>>();
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            ChatbotForDNCEng bot = new ChatbotForDNCEng(mockLogger.Object, mockConfiguration.Object);
+
+            // Create conversationUpdate activity
+            Activity conversationUpdateActivity = new Activity
+            {
+                Type = ActivityTypes.ConversationUpdate,
+                MembersAdded = new List<ChannelAccount>
+                {
+                    new ChannelAccount { Id = "theUser" },
+                },
+                Recipient = new ChannelAccount { Id = "theBot" },
+            };
+            TestAdapter testAdapter = new TestAdapter(Channels.Test);
+
+            // Act
+            // Send the conversation update activity to the bot.
+            await testAdapter.ProcessActivityAsync(conversationUpdateActivity, bot.OnTurnAsync, CancellationToken.None);
+
+            // Expected response
+            Attachment expected = ChatbotForDNCEng.CreateAdaptiveCardAttachment(_cards["WelcomeCard"]);
+
+            // Assert we got the welcome card
+            IMessageActivity reply = (IMessageActivity)testAdapter.GetNextReply();
+            Assert.Equal(1, reply.Attachments.Count);
+            Assert.Equal("application/vnd.microsoft.card.adaptive", reply.Attachments.FirstOrDefault()?.ContentType);
+            Assert.Equal(expected.Content, reply.Attachments.FirstOrDefault()?.Content);
+
+            // Assert we got Suggested Actions
+            SuggestedActions expectedActions = ChatbotForDNCEng.CreateSuggestedActions();
+            Assert.IsType<SuggestedActions>(reply.SuggestedActions);
+            Assert.Equal(expectedActions.Actions.Count, reply.SuggestedActions.Actions.Count);
+        }
+
+        [Theory]
+        [InlineData("Feedback", "FeedbackCard")]
+        [InlineData("Contact", "ContactSheet")]
+        public async Task ReturnsCorrectCard(string userMessage, string expectdCard)
+        {
+            // Arrange
+            Mock<ILogger<ChatbotForDNCEng>> mockLogger = new Mock<ILogger<ChatbotForDNCEng>>();
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            ChatbotForDNCEng bot = new ChatbotForDNCEng(mockLogger.Object, mockConfiguration.Object);
+
+            TestAdapter testAdapter = new TestAdapter(Channels.Test);
+
+            // Act
+            // Send the conversation update activity to the bot.
+            await testAdapter.SendTextToBotAsync(userMessage, bot.OnTurnAsync, CancellationToken.None);
+
+            // Expected response
+            Attachment expected = ChatbotForDNCEng.CreateAdaptiveCardAttachment(_cards[expectdCard]);
+
+            // Assert we got the correct card
+            IMessageActivity reply = (IMessageActivity)testAdapter.GetNextReply();
+            Assert.Equal(1, reply.Attachments.Count);
+            Assert.Equal("application/vnd.microsoft.card.adaptive", reply.Attachments.FirstOrDefault()?.ContentType);
+            Assert.Equal(expected.Content, reply.Attachments.FirstOrDefault()?.Content);
+
+            // Assert we got Suggested Actions
+            reply = (IMessageActivity)testAdapter.GetNextReply();
+            SuggestedActions expectedActions = ChatbotForDNCEng.CreateSuggestedActions();
+            Assert.IsType<SuggestedActions>(reply.SuggestedActions);
+            Assert.Equal(expectedActions.Actions.Count, reply.SuggestedActions.Actions.Count);
+        }
+
+        [Theory]
+        [InlineData("What is Known issues?")]
+        [InlineData("How do I use Helix job sender?")]
+        public async Task ReturnsAIResponse(string userMessage)
+        {
+            // Arrange
+            Mock<ILogger<ChatbotForDNCEng>> mockLogger = new Mock<ILogger<ChatbotForDNCEng>>();
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.Setup(x => x["KeyVaultName"]).Returns("DncengChatbotKV");
+            mockConfiguration.Setup(x => x["MicrosoftAppId"]).Returns("ed9b4931-f097-4f9b-ae95-cec8c7f06dd8");
+            ChatbotForDNCEng bot = new ChatbotForDNCEng(mockLogger.Object, mockConfiguration.Object);
+
+            TestAdapter testAdapter = new TestAdapter(Channels.Test);
+
+            // Act
+            // Send the conversation update activity to the bot.
+            await testAdapter.SendTextToBotAsync(userMessage, bot.OnTurnAsync, CancellationToken.None);
+
+            // Expected response
+            string notExpected = "The requested information is not available in the retrieved data. Please try another query or topic.";
+
+            // Assert the response doesn't contain "I don't know"
+            IMessageActivity reply = (IMessageActivity)testAdapter.GetNextReply();
+            Assert.Equal(1, reply.Attachments.Count);
+            Assert.Equal("application/vnd.microsoft.card.adaptive", reply.Attachments.FirstOrDefault()?.ContentType);
+            Assert.DoesNotContain(notExpected, reply.Attachments.FirstOrDefault()?.Content.ToString());
+
+            // Assert we got Suggested Actions
+            reply = (IMessageActivity)testAdapter.GetNextReply();
+            SuggestedActions expectedActions = ChatbotForDNCEng.CreateSuggestedActions();
+            Assert.IsType<SuggestedActions>(reply.SuggestedActions);
+            Assert.Equal(expectedActions.Actions.Count, reply.SuggestedActions.Actions.Count);
+        }
+
+        [Theory]
+        [InlineData("Who is Abraham Lincoln?")]
+        [InlineData("How do I make cheese at home?")]
+        public async Task ReturnsCanNotFindResponse(string userMessage)
+        {
+            // Arrange
+            Mock<ILogger<ChatbotForDNCEng>> mockLogger = new Mock<ILogger<ChatbotForDNCEng>>();
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.Setup(x => x["KeyVaultName"]).Returns("DncengChatbotKV");
+            mockConfiguration.Setup(x => x["MicrosoftAppId"]).Returns("ed9b4931-f097-4f9b-ae95-cec8c7f06dd8");
+            ChatbotForDNCEng bot = new ChatbotForDNCEng(mockLogger.Object, mockConfiguration.Object);
+
+            TestAdapter testAdapter = new TestAdapter(Channels.Test);
+
+            // Act
+            // Send the conversation update activity to the bot.
+            await testAdapter.SendTextToBotAsync(userMessage, bot.OnTurnAsync, CancellationToken.None);
+
+            // Expected response
+            string expected = "The requested information is not available in the retrieved data. Please try another query or topic.";
+
+            // Assert we got "I don't know"
+            IMessageActivity reply = (IMessageActivity)testAdapter.GetNextReply();
+            Assert.Equal(1, reply.Attachments.Count);
+            Assert.Equal("application/vnd.microsoft.card.adaptive", reply.Attachments.FirstOrDefault()?.ContentType);
+            Assert.Contains(expected, reply.Attachments.FirstOrDefault()?.Content.ToString());
+
+            // Assert we got Suggested Actions
+            reply = (IMessageActivity)testAdapter.GetNextReply();
+            SuggestedActions expectedActions = ChatbotForDNCEng.CreateSuggestedActions();
+            Assert.IsType<SuggestedActions>(reply.SuggestedActions);
+            Assert.Equal(expectedActions.Actions.Count, reply.SuggestedActions.Actions.Count);
+        }
+    }
+}
---
+++ src/Chatbot/Chatbot/ChatbotForDNCEng.cs
@@ -0,0 +1,286 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.22.0
+using Newtonsoft.Json;
+using System;
+using System.ClientModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using Azure.AI.OpenAI;
+using Azure.AI.OpenAI.Chat;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OpenAI.Chat;
+
+
+namespace Chatbot
+{
+    public class ChatbotForDNCEng : ActivityHandler
+    {
+        private readonly Dictionary<string, string> _cards = new Dictionary<string, string>()
+        {
+            {"FeedbackCard", Path.Combine(".", "Resources", "FeedbackCard.json")},
+            {"ContactSheet", Path.Combine(".", "Resources", "ContactSheet.json")},
+            {"WelcomeCard", Path.Combine(".", "Resources", "WelcomeCard.json")}
+        };
+
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+
+        public ChatbotForDNCEng(ILogger<ChatbotForDNCEng> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Bot is handling member added.");
+            await SendWelcomeMessageAsync(turnContext, cancellationToken);
+        }
+
+        public async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Bot is sending welcome message.");
+            Attachment cardAttachment = CreateAdaptiveCardAttachment(_cards["WelcomeCard"]);
+            
+            foreach (var member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    IMessageActivity welcomeMessage = MessageFactory.Attachment(cardAttachment);
+                    welcomeMessage.SuggestedActions = CreateSuggestedActions();
+                    await turnContext.SendActivityAsync(welcomeMessage, cancellationToken);
+                }
+            }
+        }
+
+        // From bot framework samples
+        public static SuggestedActions CreateSuggestedActions()
+        {
+            SuggestedActions actions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "Give feedback", Type = ActionTypes.ImBack, Value = "Feedback" } ,
+                    new CardAction() { Title = "Who do I contact for...", Type = ActionTypes.ImBack, Value = "Contact" },
+
+                },
+            };
+            return actions;
+        }
+
+        // This method allows the bot to respond to a user message
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Bot is handling message activity.");
+            // Error handling
+            ArgumentNullException.ThrowIfNull(turnContext);
+
+            // Get user input
+            String option = turnContext.Activity.Text.ToLower();
+            String userRequest = turnContext.Activity.Text;
+
+            const String followup = "Is there anything else I can help you with today?";
+
+            switch (option)
+            {
+                case "feedback":
+                    Attachment cardAttachment = CreateAdaptiveCardAttachment(_cards["FeedbackCard"]);
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(cardAttachment), cancellationToken);
+                    break;
+                case "contact":
+                    Attachment contactAttachment = CreateAdaptiveCardAttachment(_cards["ContactSheet"]);
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(contactAttachment), cancellationToken);
+                    break;
+                default:
+                    Attachment aiResponse = await AskOpenAI(userRequest);
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(aiResponse), cancellationToken);
+                    break;
+            }
+            IMessageActivity followupActions = MessageFactory.Text(followup);
+            followupActions.SuggestedActions = CreateSuggestedActions();
+            await turnContext.SendActivityAsync(followupActions, cancellationToken);
+        }
+
+        // From bot framework samples
+        public static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            String adaptiveCardJson = File.ReadAllText(filePath);
+            Attachment adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment FormatLinks(String response, List<(String, String)> links)
+        {
+            // This method formats the Azure OpenAI answer and citations as an attachment to send back to the user
+            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+
+            card.Body.Add(new AdaptiveTextBlock()
+            {
+                Text = response,
+                Size = AdaptiveTextSize.Default,
+                Wrap = true,
+            });
+
+            foreach (var link in links) 
+            {
+                AdaptiveOpenUrlAction action = new AdaptiveOpenUrlAction()
+                {
+                    Title = link.Item1,
+                    Url = new Uri(link.Item2),
+                };
+                card.Actions.Add(action);
+            }
+
+            string adaptiveCardJson = card.ToJson();
+
+            Attachment adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+
+            return adaptiveCardAttachment;
+        }
+
+        public async Task<Attachment> AskOpenAI(String question)
+        {
+            _logger.LogDebug("Bot is making a request to Azure OpenAI.");
+            /* 
+             * The line below disables the warning because the.AddDataSource
+             * is an experimental feature a part of the newest release.
+             *
+             * Source: https://learn.microsoft.com/en-us/azure/ai-services/openai/use-your-data-quickstart?tabs=command-line%2Cpython-new&pivots=programming-language-csharp
+             */
+
+#pragma warning disable AOAI001
+
+            String servicePrompt = "You are an AI assistant for Microsoft’s .NET Engineering team" +
+                                "that helps the team and other Microsoft employees find information using " +
+                                ".NET’s repositories on GitHub. You like to give examples whenever possible." +
+                                "You cite your references with the filepath in the format of [filepath].";
+
+            ChatClient chatClient = await CreateChatClient();
+            ChatCompletionOptions chatCompletionsOptions = await ConfigChatOptions();
+
+            List<ChatMessage> messages = new List<ChatMessage>
+            { 
+                // If there is old chat history that you want to include, you would do it here
+                // Adds the service prompt, gives context to the bot on how it should respond
+                new SystemChatMessage(servicePrompt),
+                // Adds the user's question
+                new UserChatMessage(question),
+            };
+
+            ChatCompletion completion = await chatClient.CompleteChatAsync(messages, chatCompletionsOptions);
+
+            List<(String, String)> citationInfo = GetCitations(completion);
+
+            Attachment responseCard = FormatLinks(completion.Content[0].Text, citationInfo);
+            return responseCard;
+
+        }
+
+        public List<(String, String)> GetCitations(ChatCompletion completion)
+        {
+            if (completion.Content[0].Text.Contains("The requested information is not available in the retrieved data. Please try another query or topic."))
+            {
+                return new List<(string, string)> { };
+            }
+            // Gets the message context: contains the citations, convo intent, and info about retrieved docs
+            AzureChatMessageContext messageContext = completion.GetAzureMessageContext();
+            IReadOnlyList<AzureChatCitation> citations = messageContext.Citations;
+
+            int currentCitation = 1;
+            List<(String, String)> citationInfo = new List<(String, String)>();
+            foreach (var citation in citations)
+            {
+                String citationTitle = "Doc " + currentCitation.ToString() + ": " + citation.Filepath;
+                citationInfo.Add((citationTitle, GetGithubUrl(citation.Url)));
+                currentCitation++;
+            }
+            return citationInfo;
+        }
+
+        public String GetGithubUrl(string blobUrl)
+        {
+            if (string.IsNullOrEmpty(blobUrl))
+            {
+                return "";
+            }
+            
+            String githubPath = blobUrl.Split("https://explorerstestdata.blob.core.windows.net/automationdemo/")[1];
+            String[] pathParts = githubPath.Split("/", 2);
+            String repo = pathParts[0];
+            String path = pathParts[1];
+
+            return $"https://github.com/dotnet/{repo}/blob/main/{path}";
+        }
+
+        public async Task<ChatClient> CreateChatClient()
+        {
+            _logger.LogDebug("Bot is creating chat client.");
+            String openAIEndpoint = "https://testing-bot.openai.azure.com/";
+            String openAIKey = await GetSecrets("AzureOpenAiApiKey");
+            String openAIDeploymentName = "explorers-test";
+
+            AzureOpenAIClient azureClient = new(new Uri(openAIEndpoint), new ApiKeyCredential(openAIKey));
+
+            ChatClient chatClient = azureClient.GetChatClient(openAIDeploymentName);
+            _logger.LogDebug("Bot successfully created chat client.");
+
+            return chatClient;
+        }
+
+        public async Task<ChatCompletionOptions> ConfigChatOptions()
+        {
+            _logger.LogDebug("Bot is configuring search options.");
+            String searchEndpoint = "https://testingbot-search.search.windows.net";
+            String searchKey = await GetSecrets("AiSearchApiKey");
+            String searchIndex = "all-data-auto-uploaded-daily";
+
+            // Configure the chat completions options to use our data instead of ChatGPT's training data
+            ChatCompletionOptions chatCompletionsOptions = new ChatCompletionOptions();
+            chatCompletionsOptions.AddDataSource(new AzureSearchChatDataSource()
+            {
+                Endpoint = new Uri(searchEndpoint),
+                IndexName = searchIndex,
+                Authentication = DataSourceAuthentication.FromApiKey(searchKey),
+            });
+            _logger.LogDebug("Bot successfully created search options.");
+
+            return chatCompletionsOptions;
+        }
+
+        private async Task<String> GetSecrets(String secretName)
+        {
+            _logger.LogDebug("Bot is getting secrets.");
+            String keyVaultName = _configuration["KeyVaultName"];
+            String kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+            DefaultAzureCredential credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+            {
+                ManagedIdentityClientId = _configuration["MicrosoftAppId"]
+            }
+            ); 
+            SecretClient client = new SecretClient(new Uri(kvUri), credential);
+            _logger.LogDebug("Bot created secret client.");
+
+            KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+            _logger.LogDebug("Bot is retrieved secret successfully.");
+
+            return secret.Value;
+        }
+    }
+}
