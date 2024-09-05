79c393ebb442f182fdc3e7be81deebbd98c6db94
Cali Kuang <t-calikuang@microsoft.com>
2024-09-05 15:41:15 -0700

    WIP - Adding a web api to generate tokens, creating website for the bot

---
+++ src/Chatbot/WebAPI/Models/DirectLineTokenDetails.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Models
+{
+    public class DirectLineTokenDetails
+    {
+        public string Token { get; set; }
+
+        public int ExpiresIn { get; set; }
+
+        public string ConversationId { get; set; }
+    }
+}
