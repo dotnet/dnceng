3a9f2c0d576190369b97680d10673b3fafd6dd9f
missymessa <mjanecke@microsoft.com>
2025-04-29 14:43:51 -0700

    Updated to use .NET8; removed unused usings

--- src/Telemetry/AzureDevOpsTimeline/Program.cs
+++ src/Telemetry/AzureDevOpsTimeline/Program.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Kusto.Cloud.Platform.Utils;
 using Microsoft.DncEng.Configuration.Extensions;
 using Microsoft.DotNet.Internal.AzureDevOps;
 using Microsoft.DotNet.ServiceFabric.ServiceHost;
